29cedf34cd6a134b9e57376cef4842e1e52aceda
Ron Liu <ron.liu.dev@gmail.com>
2020-12-29 21:27:52 +1100

    fix: build issue

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,35 +3,35 @@ module.exports = {
     title: `Ron Liu Blog`,
     author: {
       name: `Ron Liu`,
-      summary: `father of a daughter, fullstack dev`
+      summary: `father of a daughter, fullstack dev`,
     },
     description: `Blog recording what I am thinking, doing, learning...`,
     siteUrl: `https://www.rontech.com.au/`,
     social: {
-      twitter: `ronliu2k`
-    }
+      twitter: `ronliu2k`,
+    },
   },
   plugins: [
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         path: `${__dirname}/content/blog`,
-        name: `blog`
-      }
+        name: `blog`,
+      },
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         path: `${__dirname}/content/learned-today`,
-        name: `learned-today`
-      }
+        name: `learned-today`,
+      },
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         path: `${__dirname}/content/assets`,
-        name: `assets`
-      }
+        name: `assets`,
+      },
     },
     {
       resolve: `gatsby-transformer-remark`,
@@ -40,20 +40,20 @@ module.exports = {
           {
             resolve: `gatsby-remark-images`,
             options: {
-              maxWidth: 590
-            }
+              maxWidth: 590,
+            },
           },
           {
             resolve: `gatsby-remark-responsive-iframe`,
             options: {
-              wrapperStyle: `margin-bottom: 1.0725rem`
-            }
+              wrapperStyle: `margin-bottom: 1.0725rem`,
+            },
           },
           `gatsby-remark-prismjs`,
           `gatsby-remark-copy-linked-files`,
-          `gatsby-remark-smartypants`
-        ]
-      }
+          `gatsby-remark-smartypants`,
+        ],
+      },
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
@@ -61,7 +61,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         trackingId: `UA-167947934-1`,
-      }
+      },
     },
     `gatsby-plugin-feed`,
     {
@@ -73,19 +73,19 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        // icon: `content/assets/gatsby-icon.png`
-      }
+        icon: `content/assets/blogger-logo.png`,
+      },
     },
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-plugin-typography`,
       options: {
-        pathToConfigModule: `src/utils/typography`
-      }
+        pathToConfigModule: `src/utils/typography`,
+      },
     },
     `gatsby-plugin-graphql-codegen`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
-  ]
+  ],
 }
